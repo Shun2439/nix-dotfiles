18770908f7efeb3a5dd43558c728ec6063fa56eb
Shun2439 <shunta.kobayashi24@gmail.com>
2025-02-20 08:00:49 +0900

    snapshot / add minimal xmonad

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,21 +1,21 @@
 { config, pkgs, ... }:
 {
   imports = [
-    # ./configs/nixos/core/nixos.nix
-    # ./configs/home-manager/development.nix
-    # ./configs/home-manager/vim
-    ./configs/home-manager/tmux
-    # ./configs/browser
-    ./configs/home-manager/git
-    # ./configs/wezterm
-    ./configs/home-manager/eza
-    ./configs/home-manager/fzf
+    ../../configs/nixos/core/nixos.nix
+    # ../../configs/home-manager/development.nix
+    # ../../configs/home-manager/vim
+    ../../configs/home-manager/tmux
+    # ../../configs/home-manager/browser
+    ../../configs/home-manager/git
+    ../../configs/home-manager/wezterm
+    ../../configs/home-manager/eza
+    ../../configs/home-manager/fzf
   ];
 
   home = rec {
-    # username = "demo";
-    # homeDirectory = "/home/${username}";
-    # stateVersion = "22.11";
+    username = "shun2439";
+    homeDirectory = "/home/${username}";
+    stateVersion = "22.11";
 
     packages = with pkgs; [
       bat
