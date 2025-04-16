0b2ece96cbb2338ad51cc4a6c32be5d526dd1b64
Shun2439 <shunta.kobayashi24@gmail.com>
2025-04-17 08:08:50 +0900

    fix ws-move

--- configs/home-manager/hyprland/key-binds.nix
+++ configs/home-manager/hyprland/key-binds.nix
@@ -2,30 +2,56 @@
 let
   open-terminal = 
     pkgs.writeScriptBin "open-terminal"
-    # bash
-    ''
-    hyprctl dispatch exec wezterm
-    '';
+      # bash
+      ''
+      hyprctl dispatch exec wezterm
+      '';
   ws-switch =
     pkgs.writeScriptBin "ws-switch"
-    # bash
-    ''
-      monitor=$(hyprctl activeworkspace -j | jq .monitorID)
-      hyprctl dispatch workspace $(($monitor * 10 + $1))
-    '';
+      # bash
+      ''
+        monitor=$(hyprctl activeworkspace -j | jq .monitorID)
+        hyprctl dispatch workspace $(($monitor * 10 + $1))
+      '';
   ws-move =
-    pkgs.writeScriptBin "ws-switch"
-    # bash
-    ''
-      monitor=$(hyprctl activeworkspace -j | jq .monitorID)
-      hyprctl dispatch movetoworkspace $(($monitor * 10 + $1))
-    '';
+    pkgs.writeScriptBin "ws-move"
+      # bash
+      ''
+        monitor=$(hyprctl activeworkspace -j | jq .monitorID)
+        hyprctl dispatch movetoworkspace $(($monitor * 10 + $1))
+      '';
   open-wofi =
     pkgs.writeScriptBin "open-wofi"
-      # bash
-    ''
-      wofi --show drun --width 512px
-    '';
+        # bash
+      ''
+        wofi --show drun --width 512px
+      '';
+  # monitor-switch = 
+  #   pkgs.writeScriptBin "monitor-switch"
+  #   # bash
+  #   ''
+  #     #!/bin/bash
+
+  #     PRIMARY="LVDS-1"
+  #     EXTERNAL="VGA-1"
+
+  #     PRIMARY_STATE=$(hyprctl monitors all | grep "$PRIMARY" | grep -oE "(enabled|disabled)")
+  #     EXTERNAL_STATE=$(hyprctl monitors all | grep "$EXTERNAL" | grep -oE "(enabled|disabled)")
+
+  #     if [[ "$PRIMARY_STATE" == "enabled" && "$EXTERNAL_STATE" == "enabled" ]]; then
+  #       # Extend mode, switch to external only
+  #       hyprctl keyword monitor "$PRIMARY",disable
+  #       hyprctl keyword monitor "$EXTERNAL",preferred,auto,1
+  #     elif [[ "$PRIMARY_STATE" == "disabled" && "$EXTERNAL_STATE" == "enabled" ]]; then
+  #       # External only, switch to primary only
+  #       hyprctl keyword monitor "$PRIMARY",preferred,auto,1
+  #       hyprctl keyword monitor "$EXTERNAL",disable
+  #     else
+  #       # Primary only or other state, switch back to extend
+  #       hyprctl keyword monitor "$PRIMARY",preferred,0x0,1
+  #       hyprctl keyword monitor "$EXTERNAL",preferred,auto,1920x0,1 # Adjust position as needed
+  #     fi
+  #   '';
 in
 {
   wayland.windowManager.hyprland.settings = {
@@ -53,11 +79,14 @@ in
       "$mainMod SHIFT, left, movetoworkspace, m-1"
 
       # system
-      "$mainMod, x, exec, systemctl suspend"
+      "$mainMod, l, exec, systemctl suspend"
 
       # launcher
       "$mainMod SHIFT, P, exec, ${open-wofi}/bin/open-wofi"
       "$mainMod, period, exec, wofi-emoji"
+
+      # monitor
+      # "$mainMod, p, exec, ${monitor-switch}/bin/monitor-switch"
     ] ++ (
       lib.flatten (
         lib.genList (n:
