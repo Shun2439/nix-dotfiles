0912bec80e5418f46f284ad294430ac6cd5ab1c3
Shun2439 <shunta.kobayashi24@gmail.com>
2026-06-08 15:55:55 +0900

    chore: add ghostty config

--- configs/home-manager/ghostty/default.nix
+++ configs/home-manager/ghostty/default.nix
@@ -5,6 +5,11 @@
     settings = {
       font-family = "Moralerspace Radon HW";
       theme = "Catppuccin Mocha";
+      background-opacity = 0.75;
+      background-blur = true;
+      window-theme = "ghostty";
+      copy-on-select = true;
+      clipboard-write = "allow";
     };
   };
 }
