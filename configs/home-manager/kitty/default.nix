818c324c06a7048474168db755fca67bed5e20f0
Shun2439 <shunta.kobayashi24@gmail.com>
2026-07-02 22:07:07 +0900

    chore: add kitty config

--- configs/home-manager/kitty/default.nix
+++ configs/home-manager/kitty/default.nix
@@ -4,6 +4,18 @@
     enable = true;
     font.name = "Moralerspace Radon HW";
     shellIntegration.enableFishIntegration = true;
-    theme = "Catppuccin-Mocha";
+    enableGitIntegration = true;
+    themeFile = "Catppuccin-Mocha";
+    settings = {
+      hide_window_decorations = true;
+
+      tab_bar_style = "powerline";
+      tab_powerline_style = "round";
+      tab_bar_edge = "top";
+
+      cursor_trail = 200;
+      cursor_trail_decay = "0.1 0.4";
+      cursor_trail_start_threshold = 2;
+    };
   };
 }
